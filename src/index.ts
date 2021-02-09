90df18fe34280f89b0aac4e4d6687e5bfce93958
nanangqq <gingggg@gmail.com>
2021-02-09 12:27:46 +0000

    start

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// console.log('hi')
+
+
+const sayHi = (name:string, age:number, gender:string):string => `Hello ${name}, ${age}, ${gender}`
+
+const name = 'a', age = 33, gender = 'male'
+console.log(sayHi(name, age, gender))
+
+export {}
